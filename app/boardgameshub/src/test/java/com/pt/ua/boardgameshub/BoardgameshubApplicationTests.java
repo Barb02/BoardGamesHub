72756aedc7c9f090dc2dcd2c2d75950ff9d64d3f
Bárbara Galiza <barnobregali02@gmail.com>
2023-11-16 14:33:20 +0000

    added docker config and spring initializer files

---
+++ app/boardgameshub/src/test/java/com/pt/ua/boardgameshub/BoardgameshubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pt.ua.boardgameshub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardgameshubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
