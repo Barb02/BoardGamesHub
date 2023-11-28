737de1fc6e88bee14a5495c3e4a6e7d8d3be33bb
Tomas Victal <tomasvictal@gmail.com>
2023-11-28 14:47:10 +0000

    created service of game

---
+++ app/boardgamehubWeb/src/services/gameService.jsx
@@ -0,0 +1,11 @@
+import { createClient } from "./client";
+
+const client = createClient("localhost:8080/api/v1")
+
+const gameService = {
+    async getGame(id){
+        return await client.get(`/game/${id}`)
+    },
+}
+
+export default gameService;
