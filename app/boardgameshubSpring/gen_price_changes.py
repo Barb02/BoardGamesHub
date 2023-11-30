37c5558b9acc31127e1655ffa2cb319980e77abf
Bárbara Galiza <barnobregali02@gmail.com>
2023-11-30 12:37:20 +0000

    data generation working

--- app/boardgameshubSpring/gen_price_changes.py
+++ app/boardgameshubSpring/gen_price_changes.py
@@ -1,25 +1,32 @@
 import requests
 import random
 import json
+import time
 
-NUM_GAMES = 30
+NUM_GAMES = 2
 NUM_STORES = 4
+DELAY = 0.5 * 60
 
-changes = [0.1, 1.1, 0.2, 1.2, 0.3, 1.3, 0.4, 1.4, 0.5, 1.5, 0.6, 1.6, 0.7, 1.7, 0.8, 1.8, 0.9, 1.9]
-prob_weights = [20, 20, 10, 10, 10, 10, 5, 5, 5, 5, 2, 2, 2, 2, 1, 1, 1, 1]
+changes = [0.95, 1.005, 0.9, 1.1, 0.8, 1.2, 0.7, 1.3, 0.6, 1.4, 0.5, 1.5, 0.4, 1.6, 0.3, 1.7, 0.2, 1.8]
+prob_weights = [50, 50, 30, 30, 30, 30, 20, 20, 20, 20, 5, 5, 5, 5, 1, 1, 1, 1]
 message = {}
 
 while True:
     game_id = random.randint(1, NUM_GAMES)
     store_id = random.randint(1, NUM_STORES)
-    r = requests.get("localhost:8080/api/v1/price/" + game_id + "/" + store_id)
+    r = requests.get("http://localhost:8080/api/v1/price/" + str(game_id) + "/" + str(store_id))
     if r.status_code == 200:
-        change = random.choice(changes, p=prob_weights, k=1)
+        change = random.choices(changes, weights=prob_weights, k=1)[0]
+        print("Curr price:" + str(r.json()['price']))
         new_price = r.json()['price'] * change
+        print("Change:" + str(change))
+        print("New price:" + str(new_price))
         message['game_id'] = game_id
         message['store_id'] = store_id
         message['price'] = new_price
         msg_json = json.dumps(message)
+        print(msg_json)
+    time.sleep(DELAY)
 
 
 
