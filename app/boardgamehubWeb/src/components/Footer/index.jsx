8e1454e886dd61fcd16919aa9a608a3cd2602d9a
pdPinho <pd.pinho@ua.pt>
2023-12-01 17:23:55 +0000

    updated footer, created product component and search structure

--- app/boardgamehubWeb/src/components/Footer/index.jsx
+++ app/boardgamehubWeb/src/components/Footer/index.jsx
@@ -2,7 +2,7 @@ import { FaGithub } from "react-icons/fa";
 
 function Footer() {
     return (
-    <footer class="w-full bg-navbar">
+    <footer class="w-full bg-navbar inset-x-0 bottom-0">
         <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
         <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 BoardGamesHub™. All Rights Reserved.
         </span>
