198c2e5de18f5d71162144cb8aaaf3be65af3287
Bárbara Galiza <barnobregali02@gmail.com>
2023-12-01 17:07:07 +0000

    changed java version to 17 and springboot to 3.1.4, and added doc

--- app/boardgameshubSpring/src/main/java/com/pt/ua/boardgameshub/domain/jpa_domain/Designer.java
+++ app/boardgameshubSpring/src/main/java/com/pt/ua/boardgameshub/domain/jpa_domain/Designer.java
@@ -1,10 +1,10 @@
 package com.pt.ua.boardgameshub.domain.jpa_domain;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
-import javax.persistence.Table;
-import javax.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
 
 import java.util.HashSet;
 import java.util.Set;
