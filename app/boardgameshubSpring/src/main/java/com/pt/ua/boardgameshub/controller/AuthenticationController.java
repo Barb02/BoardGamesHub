8001e8fdafa69957f260620e96d799ad732b14cb
Bárbara Galiza <barnobregali02@gmail.com>
2023-12-06 17:00:37 +0000

    added documentation for the new endpoints

--- app/boardgameshubSpring/src/main/java/com/pt/ua/boardgameshub/controller/AuthenticationController.java
+++ app/boardgameshubSpring/src/main/java/com/pt/ua/boardgameshub/controller/AuthenticationController.java
@@ -5,14 +5,22 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.pt.ua.boardgameshub.dao.request_body.SignUpRequest;
 import com.pt.ua.boardgameshub.dao.request_body.SignInRequest;
 import com.pt.ua.boardgameshub.dao.response_body.JwtAuthenticationResponse;
+import com.pt.ua.boardgameshub.domain.jpa_domain.Game;
 import com.pt.ua.boardgameshub.service.jpa_service.AuthenticationService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 
+@CrossOrigin(maxAge = 3600)
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
@@ -20,12 +28,21 @@ public class AuthenticationController {
     
     private final AuthenticationService authenticationService;
 
-    // TODO: Documentation
+    @Operation(summary = "Signup the given user to the website")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "User was registered",
+            content = {@Content(mediaType = "application/json", schema = @Schema(implementation = Game.class))}),
+            @ApiResponse(responseCode = "500", description = "Couldn't register user", content = @Content)})
     @PostMapping("/signup")
     public ResponseEntity<JwtAuthenticationResponse> signup(@RequestBody SignUpRequest request) {
         return ResponseEntity.ok(authenticationService.signup(request));
     }
 
+    @Operation(summary = "Signin to the website with the credentials provided")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "User is signed in",
+            content = {@Content(mediaType = "application/json", schema = @Schema(implementation = Game.class))}),
+            @ApiResponse(responseCode = "500", description = "Couldn't sign-in user", content = @Content)})
     @PostMapping("/signin")
     public ResponseEntity<JwtAuthenticationResponse> signin(@RequestBody SignInRequest request) {
         return ResponseEntity.ok(authenticationService.signin(request));
