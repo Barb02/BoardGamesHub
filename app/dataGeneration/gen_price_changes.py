f8f62c0274a88d172c0ee627852af3f4d9ac9323
WildBunnie <wildbunnie2003@gmail.com>
2023-12-12 11:30:55 +0000

    changed data generation

--- app/dataGeneration/gen_price_changes.py
+++ app/dataGeneration/gen_price_changes.py
@@ -8,13 +8,12 @@ NUM_GAMES = 50
 NUM_STORES = 4
 DELAY = 10
 MAX_CLICKS = 10
+MIN_DISCOUNT = 10
+MAX_DISCOUNT = 90
 
-bootstrap_servers = 'kafka:29092'  # Replace with your Kafka broker's address
-producer = Producer({'bootstrap.servers': bootstrap_servers})
+producer = Producer({'bootstrap.servers': 'kafka:29092'})
 topic = 'bgh'
-
-changes = [0.95, 1.005, 0.9, 1.1, 0.8, 1.2, 0.7, 1.3, 0.6, 1.4, 0.5, 1.5, 0.4, 1.6, 0.3, 1.7, 0.2, 1.8]
-prob_weights = [50, 50, 30, 30, 30, 30, 20, 20, 20, 20, 5, 5, 5, 5, 1, 1, 1, 1]
+prices = {}
 
 def check_game_endpoint():
     url = 'http://springboot:8080/api/v1/price/1/1'
@@ -27,36 +26,48 @@ def check_game_endpoint():
             print("Not Available")
         time.sleep(10)
 
-# Produce asynchronously
-def delivery_report(err, msg):
-    if err is not None:
-        print(f'Message delivery failed: {err}')
+def send_message(message):
+    msg_json = json.dumps(message)
+    producer.produce(topic, value=msg_json)
+    print("Message sent",msg_json)
+
+def send_price_message():
+    message = {"type":"PRICE"}
+
+    game_id = random.randint(1, NUM_GAMES)
+    store_id = random.randint(1, NUM_STORES)
+
+    r = requests.get("http://springboot:8080/api/v1/price/" + str(game_id) + "/" + str(store_id))
+    if r.status_code != 200:
+        return
+    current_price = r.json()['price']
+
+    key = (game_id, store_id)
+    if key in prices.keys():
+        if current_price == prices[key]:
+            new_price = current_price - (current_price * (random.randint(MIN_DISCOUNT, MAX_DISCOUNT) / 100))
+        else:
+            new_price = prices[key]
     else:
-        print(f'Message delivered to {msg.topic()} [{msg.partition()}]')
+        prices[key] = current_price
+        new_price = current_price
+
+    message['game_id'] = game_id
+    message['store_id'] = store_id
+    message['price'] = new_price
+    send_message(message)
+
+def send_click_message():
+    message = {"type":"CLICK"}
+    message['game_id'] = random.randint(1, NUM_GAMES)
+    message['amount'] = random.randint(1, MAX_CLICKS)
+    send_message(message)
 
 def run():
     while True:
-        for message_type in ["PRICE", "CLICK"]:
-            message = {}
-            message['type'] = message_type
-            if message_type == "PRICE":
-                game_id = 1
-                store_id = random.randint(1, NUM_STORES)
-                r = requests.get("http://springboot:8080/api/v1/price/" + str(game_id) + "/" + str(store_id))
-                if r.status_code == 200:
-                    change = random.choices(changes, weights=prob_weights, k=1)[0]
-                    new_price = r.json()['price'] * change
-                    message['game_id'] = game_id
-                    message['store_id'] = store_id
-                    message['price'] = new_price
-
-            elif message_type == "CLICK":
-                message['game_id'] = random.randint(1, NUM_GAMES)
-                message['amount'] = random.randint(1, MAX_CLICKS)
-
-            msg_json = json.dumps(message)
-            producer.produce(topic, value=msg_json, callback=delivery_report)
-            print("Message sent",msg_json)
+        send_price_message()
+        send_click_message()
+            
         time.sleep(DELAY)
 
 def main():
