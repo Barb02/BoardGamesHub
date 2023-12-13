f0462a4ed27f17ce6b161f30a372d1f0236b4149
pdPinho <pd.pinho@ua.pt>
2023-12-13 12:17:54 +0000

    removed redundancy

--- app/boardgameshubWeb/src/components/ProductList/index.jsx
+++ app/boardgameshubWeb/src/components/ProductList/index.jsx
@@ -19,7 +19,6 @@ function ProductList({ query }) {
     }
 
     useEffect(() => {
-        console.log("query is " + query);
         if (query || query === ""){
             gameService.getGames(query).then((data) => {
                 setRdata(data || []);
