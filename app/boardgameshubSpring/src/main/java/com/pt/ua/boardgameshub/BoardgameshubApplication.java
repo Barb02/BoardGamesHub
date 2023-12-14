b1df27c65f2522318739eb73766e1c0e3225ba24
Bárbara Galiza <barnobregali02@gmail.com>
2023-12-14 18:31:38 +0000

    removed unncessary signup signin on startup

--- app/boardgameshubSpring/src/main/java/com/pt/ua/boardgameshub/BoardgameshubApplication.java
+++ app/boardgameshubSpring/src/main/java/com/pt/ua/boardgameshub/BoardgameshubApplication.java
@@ -1,17 +1,14 @@
 package com.pt.ua.boardgameshub;
 
-import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
@@ -20,9 +17,6 @@ import org.springframework.context.event.EventListener;
 @SpringBootApplication
 public class BoardgameshubApplication {
 
-    @Value("${admin.password}")
-    String adminPassword;
-
 	public static void main(String[] args) {
 		SpringApplication.run(BoardgameshubApplication.class, args);
 	}
@@ -30,86 +24,31 @@ public class BoardgameshubApplication {
     @EventListener(ApplicationReadyEvent.class)
     public void doSomethingAfterStartup() {
 
-		if(signin()){
+		if( ! checkDatabaseEmpty()){
 			return;
 		}
 
-        String token = signup();
-        if(token != null){
-            int bgcount = loadBoardGames(token);
-            loadStores(token);
-            loadPrices(token);
-            loadClicks(bgcount);
-        }
+        int bgcount = loadBoardGames();
+        loadStores();
+        loadPrices();
+        loadClicks(bgcount);
+        
 	}
 
-    public boolean signin() {
-        boolean siginSuccess = false;
-        try{
-            String jsonString = "{\"email\":\"admin@gmail.com\",\"password\":\""+adminPassword+"\"}";
-            JSONObject request = new JSONObject(jsonString);
-            URL url = new URL("http://localhost:8080/api/v1/auth/signin");
-            
+    public boolean checkDatabaseEmpty(){
+        try {
+            URL url = new URL("http://localhost:8080/api/v1/game/1");
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("POST");
-            connection.setRequestProperty("Content-Type", "application/json");
-            connection.setDoOutput(true);
-
-            String jsonInputString = request.toString();
-            byte[] jsonBytes = jsonInputString.getBytes(StandardCharsets.UTF_8);
-            try (DataOutputStream outputStream = new DataOutputStream(connection.getOutputStream())) {
-                outputStream.write(jsonBytes);
-                outputStream.flush();
-            }
+            connection.setRequestMethod("GET");
             int responseCode = connection.getResponseCode();
-            if (responseCode == HttpURLConnection.HTTP_OK) {
-                System.out.println("POST request sent successfully");
-                siginSuccess = true;
-            } else {
-                System.out.println("POST request failed: " + responseCode);
+			if (responseCode == HttpURLConnection.HTTP_OK) {
+                    return false;
             }
-        }
-        catch(Exception e){
+            connection.disconnect();
+        } catch (IOException e) {
             e.printStackTrace();
         }
-        return siginSuccess;
-    }
-
-    public String signup() {
-        String token = null;
-        try{
-            String jsonString = "{\"username\":\"admin\",\"email\":\"admin@gmail.com\",\"password\":\""+adminPassword+"\"}";
-            JSONObject request = new JSONObject(jsonString);
-            URL url = new URL("http://localhost:8080/api/v1/auth/signup");
-
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("POST");
-            connection.setRequestProperty("Content-Type", "application/json");
-            connection.setDoOutput(true);
-
-            String jsonInputString = request.toString();
-            byte[] jsonBytes = jsonInputString.getBytes(StandardCharsets.UTF_8);
-            try (DataOutputStream outputStream = new DataOutputStream(connection.getOutputStream())) {
-                outputStream.write(jsonBytes);
-                outputStream.flush();
-            }
-            int responseCode = connection.getResponseCode();
-            if (responseCode == HttpURLConnection.HTTP_OK) {
-                System.out.println("POST request sent successfully");
-                
-                InputStreamReader in = new InputStreamReader(connection.getInputStream());
-                BufferedReader br = new BufferedReader(in);
-                String output = br.readLine();
-                JSONObject response = new JSONObject(output);
-                token = response.getString("token");
-            } else {
-                System.out.println("POST request failed");
-            }
-        }
-        catch(Exception e){
-            e.printStackTrace();
-        }
-        return token;
+        return true;
     }
 
     public void loadClicks(int n){
@@ -128,7 +67,7 @@ public class BoardgameshubApplication {
 
     }
 
-    public int loadBoardGames(String token) {
+    public int loadBoardGames() {
         try {
             ClassLoader classLoader = getClass().getClassLoader();
             InputStream inputStream = classLoader.getResourceAsStream("db/board_games.json");
@@ -141,7 +80,7 @@ public class BoardgameshubApplication {
                 for (int i = 0; i < boardGames.length(); i++) {
                     JSONObject boardGame = boardGames.getJSONObject(i);
                     URL url = new URL("http://localhost:8080/api/v1/game/manual");
-                    sendRequest(url, boardGame, token);
+                    sendRequest(url, boardGame);
                 }
                 return boardGames.length();
             }
@@ -151,7 +90,7 @@ public class BoardgameshubApplication {
         return -1;
     }
 
-    public void loadStores(String token) {
+    public void loadStores() {
         try {
             ClassLoader classLoader = getClass().getClassLoader();
             InputStream inputStream = classLoader.getResourceAsStream("db/stores.json");
@@ -164,7 +103,7 @@ public class BoardgameshubApplication {
                 for (int i = 0; i < stores.length(); i++) {
                     JSONObject store = stores.getJSONObject(i);
                     URL url = new URL("http://localhost:8080/api/v1/store");
-                    sendRequest(url, store, token);
+                    sendRequest(url, store);
                 }
             }
         } catch (IOException e) {
@@ -172,7 +111,7 @@ public class BoardgameshubApplication {
         }
     }
 
-    public void loadPrices(String token) {
+    public void loadPrices() {
         try {
             ClassLoader classLoader = getClass().getClassLoader();
             InputStream inputStream = classLoader.getResourceAsStream("db/prices.json");
@@ -190,7 +129,7 @@ public class BoardgameshubApplication {
                     }
                     JSONObject store = stores.getJSONObject(i);
                     URL url = new URL("http://localhost:8080/api/v1/price/" + game_id + "/" + store_id);
-                    sendRequest(url, store, token);
+                    sendRequest(url, store);
                 }
             }
         } catch (IOException e) {
@@ -198,12 +137,11 @@ public class BoardgameshubApplication {
         }
     }
 
-    public void sendRequest(URL url, JSONObject object, String token){
+    public void sendRequest(URL url, JSONObject object){
         try{
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("POST");
             connection.setRequestProperty("Content-Type", "application/json");
-            connection.setRequestProperty("Authorization", "Bearer " + token);
             connection.setDoOutput(true);
 
             String jsonInputString = object.toString();
