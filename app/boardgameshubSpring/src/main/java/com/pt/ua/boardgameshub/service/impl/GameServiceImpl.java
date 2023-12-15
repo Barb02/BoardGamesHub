cced45b6c04195c24b5edb06750820188b41ba6f
WildBunnie <wildbunnie2003@gmail.com>
2023-12-15 16:32:05 +0000

    added publisher parameter to top games endpoint

--- app/boardgameshubSpring/src/main/java/com/pt/ua/boardgameshub/service/impl/GameServiceImpl.java
+++ app/boardgameshubSpring/src/main/java/com/pt/ua/boardgameshub/service/impl/GameServiceImpl.java
@@ -251,8 +251,8 @@ public class GameServiceImpl implements GameService{
 
 
     @Override
-    public List<Game> getTopGames(int limit) {
-        return gameRepository.findAllGamesOrderByClickCountDesc(limit);
+    public List<Game> getTopGames(int limit, String publisher) {
+        return gameRepository.findAllGamesOrderByClickCountDesc(limit, publisher);
     }
 
     /* @Override
