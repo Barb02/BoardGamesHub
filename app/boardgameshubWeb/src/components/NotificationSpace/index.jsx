b04164d44c9cbf5b80a5edc8b143e0743d62f83b
Tomas Victal <tomasvictal@gmail.com>
2023-12-20 12:28:17 +0000

    init

---
+++ app/boardgameshubWeb/src/components/NotificationSpace/index.jsx
@@ -0,0 +1,13 @@
+
+
+function NotificationSpace({Notifications}){
+
+    return(
+        <div className="fixed right-0 z-10 p-5 bg-black h-full w-[30%]">
+
+        </div>
+    )
+}
+
+
+export default NotificationSpace;
