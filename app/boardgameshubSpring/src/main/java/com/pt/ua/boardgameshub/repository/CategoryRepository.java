891c9fd9349b421d8d72446537a5a378d7a74311
WildBunnie <wildbunnie2003@gmail.com>
2023-12-20 16:36:05 +0000

    publisher, artists ... add their id automatically now

--- app/boardgameshubSpring/src/main/java/com/pt/ua/boardgameshub/repository/CategoryRepository.java
+++ app/boardgameshubSpring/src/main/java/com/pt/ua/boardgameshub/repository/CategoryRepository.java
@@ -6,8 +6,10 @@ import org.springframework.stereotype.Repository;
 import com.pt.ua.boardgameshub.domain.Category;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     List<Category> findAllByOrderByNameAsc();
+    public Optional<Category> findByName(String name);
 }
